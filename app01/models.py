b9b154fafb7178fb937644f85ee2f02dee131db0
hjc123186 <77856951@qq.com>
2016-06-22 23:03:35 +0800

    first commit

---
+++ app01/models.py
@@ -0,0 +1,28 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.contrib.auth.models import User
+class BBS(models.Model):
+    title = models.CharField(max_length=64)
+    summary = models.CharField(max_length=256,blank=True,null=True)
+    content = models.TextField()
+    author = models.ForeignKey('BBS_user')
+    view_count = models.IntegerField()
+    ranking = models.IntegerField()
+    created_at = models.DateField()
+    updatad_at = models.DateField()
+
+    def __unicode__(self):
+        return self.title
+
+class Category(models.Model):
+    name = models.CharField(max_length=32,unique=True)
+    administrator = models.ForeignKey('BBS_user')
+
+class BBS_user(models.Model):
+    user = models.OneToOneField(User)
+    signature = models.CharField(max_length=128,default='This guy is too lazy.')
+    photo = models.ImageField("uplooad_img/",default='upload_img/user-1.jpg')
+
+    def __unicode__(self):
+        return self.user
