45ffbdcba5a28a83bb47ec85450d1d91e6c354d4
huangjc <77856951@qq.com>
2016-06-27 21:33:29 +0800

    add2

--- app01/views.py
+++ app01/views.py
@@ -27,15 +27,17 @@ def sub_comment(request):
     # bbs_id = request.POST.get('bbs_obj.id')
     print "bbs_id:",bbs_id
 
-    comment = request.POST.get('comment_content')
-    django_comments.models.Comment.objects.create(
-        content_type_id = 7,
-        object_pk = bbs_id,
-        site_id = 1,
-        user = request.user,
-        comment = comment,
+    comment = request.POST.get('comment_content').strip()
+    print "comment:",comment
+    if comment is not  None and len(comment) > 0 :
+        django_comments.models.Comment.objects.create(
+            content_type_id = 7,
+            object_pk = bbs_id,
+            site_id = 1,
+            user = request.user,
+            comment = comment,
 
-    )
+        )
     return HttpResponseRedirect('/detail/%s' % bbs_id)
 
 @csrf_exempt
